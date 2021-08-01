58c7e64babe48412fa43a64b4ce67419892000fc
Inoxia25 <66139520+Inoxia25@users.noreply.github.com>
2021-08-01 22:17:20 +0530

    created file for middleware

--- middleware/index.js
+++ middleware/index.js
@@ -3,7 +3,6 @@ middlewareObj.isLoggedIn=function(req,res,next){
 	if(req.isAuthenticated()){
 		return next();
 	}
-	//req.flash("error","You need to be logged in first!");
 	res.redirect("./views/modal_form.ejs"); //if not logged in, go to login page
 	}
 module.exports=middlewareObj;
