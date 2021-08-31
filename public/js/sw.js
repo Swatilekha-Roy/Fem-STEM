4aecc1d023d40648242ac8207b5a3e22094a6a79
Swatilekha Roy <swatilekha.roy4@gmail.com>
2021-08-31 21:35:39 +0530

    Delete sw.js

--- public/js/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('push', event => {
-    const data = event.data.json();
-  console.log("Hi in sw");
-    self.registration.showNotification(data.title, {
-      body: 'Please join room number 123',
-    });
-  });
