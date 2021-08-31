f4643d167fdd46104248d924d189b8d256e504bd
Swatilekha Roy <swatilekha.roy4@gmail.com>
2021-08-31 21:37:33 +0530

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -26,20 +26,6 @@ $(document).ready(function(){
   }
 
 
-// AJAX refresh
-/*function loadDoc() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("notes").innerHTML =
-        this.responseText;
-      }
-    };
-    xhttp.open("GET", "ajax_info.txt", true);
-    xhttp.send();
-}*/
-
-
 // Youtube tutorials
 var player; 
       function onYouTubeIframeAPIReady(vidId) {
